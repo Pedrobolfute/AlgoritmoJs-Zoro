9c5b4786236697e12d9800867bceb50ebf284bca
Pedrobolfute <pepenike-101@hotmail.com>
2023-10-31 21:41:36 -0300

    refresh

--- src/08/main.js
+++ src/08/main.js
@@ -1,10 +1,9 @@
 // 8 - Criar um algoritmo que conte quantas vezes uma palavra aparece em uma string.
 
 let string = 'Who am I? A painful question. Damn! Why I am here? Why people live? Why I am living yet? Why I was born? Shit!'
-let word = 'am'
+let word = 'why'
 let count = 0
-let array = string.split(' ')
-console.log(array.join(' '))
+let array = string.replace(/\?/g, '').replace(/\!/g, '').split(' ')
 
 for(let i = 0; i < array.length; i++){
   if(array[i].toLocaleLowerCase() === word.toLocaleLowerCase()){
